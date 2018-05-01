fcce20ba8b958dc1df1371f975814553e49f9856
Rodrigo Garcia Topan Moreira <rodrigo@gruporesolv.com.br>
2018-05-01 17:10:11 -0300

    Identificação de Erro no Heroku

--- config/server.js
+++ config/server.js
@@ -35,16 +35,16 @@ consign()
 	.into(app);
 
 /* middleware que configura páginas de status */
-app.use(function(req, res, next){
+/*app.use(function(req, res, next){
 	res.status(404).render('errors/404');
 	next();
-});
+});*/
 
 /* middleware que configura msgs de erro internos */
-app.use(function(err, req, res, next){
+/*app.use(function(err, req, res, next){
 	res.status(500).render('errors/500');
 	next();
-});
+});*/
 
 /* exportar o objeto app */
 module.exports = app;
