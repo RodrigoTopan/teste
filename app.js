246fb84290699c88d349ac77e368e7497b677dad
Rodrigo Garcia Topan Moreira <rodrigo@gruporesolv.com.br>
2018-05-01 17:25:55 -0300

    Tentativa de identificar erro no heroku 5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-var port = process.env.PORT 
+var port = process.env.PORT; 
 app.listen(port, function(){
 	console.log('Servidor online');
 })
